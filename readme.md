294eaab1a9fb53404569d3100d4a19f30528adb3
fares mohalhel <fares.mohalhel@gmail.com>
2023-05-02 20:40:25 +0300

    added search functionality

---
+++ readme.md
@@ -0,0 +1,12 @@
+# eBooks
+
+## project description
+
+a simple web app that renders a list of cards and has the option to filter them by price or by user input (search)
+
+## technologies used in this project
+
+- react
+- tailwindcss
+- daisyui
+-
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -2,6 +2,20 @@ import { Fragment, useState } from "react";
 import Footer from "./Footer";
 import Header from "./Header";
 import Card from "./Card";
+import Search from "./Search";
+
+const data = [
+  { language: "Pascal", edition: "third", price: 20 },
+  { language: "Python", edition: "four", price: 30 },
+  { language: "SQL", edition: "second", price: 25 },
+  { language: "Java", edition: "first", price: 15 },
+  { language: "Java", edition: "second", price: 15 },
+  { language: "JavaScript", edition: "third", price: 30 },
+  { language: "Php", edition: "second", price: 35 },
+  { language: "laravel", edition: "first", price: 20 },
+  { language: "React", edition: "second", price: 26 },
+  { language: "React", edition: "first", price: 10 },
+];
 
 const Home = () => {
   const [books, setBooks] = useState([
@@ -17,10 +31,18 @@ const Home = () => {
     { language: "React", edition: "first", price: 10 },
   ]);
 
+  const searchHandler = (newState) => {
+    setBooks(newState);
+  };
+
   return (
     <Fragment>
       <Header />
-      <div className="w-full lg:w-[80vw] mx-auto grid grid-cols-1 lg:grid-cols-3 gap-x-6 gap-y-6 py-12">
+      <div className="mx-auto flex items-center justify-center">
+        <Search updateState={searchHandler} books={data} />
+      </div>
+
+      <div className="w-full lg:w-[80vw] mx-auto grid grid-cols-1 lg:grid-cols-3 gap-x-6 gap-y-6 place-items-center py-12">
         {books.map((book) => (
           <Card
             key={Math.random()}
